d745352e44c8ba715e20178ff89ef4c5f978d202
Nirmal-Kharal <nirmalkharal40@gmail.com>
2026-01-26 18:39:54 +0545

    feat: added sign-in page sing-up page

--- proxy.js
+++ proxy.js
@@ -1,6 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
